d1163cbed0eec202350d31e304ec158bbc166309
Hsing-Chen <tonys61311@yahoo.com.tw>
2025-07-01 14:36:21 +0800

    vite.config 設定 base: env.VITE_BASE_URL

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
   return {
-    base: '/tinder-clone/',
+    base: env.VITE_BASE_URL || '/',
     plugins: [
       vue(),
     ],
